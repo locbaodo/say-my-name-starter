79c131a10d1e5e145c3da93e19a509deb03c9293
locbaodo <locbaodo@gmail.com>
2024-03-31 17:13:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# say-my-name-starter
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/locbaodo/say-my-name-starter)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Returns a string 'Hi! My name is <firstName> <lastName>'.
+// @param fromPerson The name of the person that says "Hello!".
+// @param toPerson The name of the person that the other person says "Hello!" to.
+function sayMyName(firstName, lastName) {
+  // TODO: Finish the right side of the next line. Do not change anything else.
+  let result = '';
+  return result;
+}
+
+sayMyName('Jane', 'Doe'); // Should return 'Hi! My name is Jane Doe'
+
+// DO NOT CHANGE ANYTHING AFTER THIS LINE.
+module.exports = sayMyName;
